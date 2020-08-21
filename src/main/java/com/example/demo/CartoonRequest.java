195675bdca805bef9328d9c0e8ed7353e6d2537c
patpatpat123 <43899031+patpatpat123@users.noreply.github.com>
2020-08-22 02:16:10 +0800

    Update CartoonRequest.java

--- src/main/java/com/example/demo/CartoonRequest.java
+++ src/main/java/com/example/demo/CartoonRequest.java
@@ -2,12 +2,38 @@ package com.example.demo;
 
 import java.util.List;
 
-import lombok.Builder;
-import lombok.Data;
-	
-	@Data
-	@Builder
-	public class CartoonRequest {
-		private String cartoon;
-		private List<String> characterNames;
-	}
+public class CartoonRequest {
+
+    private String cartoon;
+    private List<String> characterNames;
+
+    public CartoonRequest(String cartoon, List<String> characterNames) {
+        this.cartoon = cartoon;
+        this.characterNames = characterNames;
+    }
+
+    public String getCartoon() {
+        return cartoon;
+    }
+
+    public void setCartoon(String cartoon) {
+        this.cartoon = cartoon;
+    }
+
+    public List<String> getCharacterNames() {
+        return characterNames;
+    }
+
+    public void setCharacterNames(List<String> characterNames) {
+        this.characterNames = characterNames;
+    }
+
+    @Override
+    public String toString() {
+        return "CartoonRequest{" +
+                "cartoon='" + cartoon + '\'' +
+                ", characterNames=" + characterNames +
+                '}';
+    }
+
+}
